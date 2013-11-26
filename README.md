049048f3e97922c19233a971307e2c8aac4511f5
Adrian Sieradzki <sarconet@hotmail.com>
2013-11-26 18:50:27 -0300

    [Test with package.json]

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-azurenodejstest
-===============
+﻿# NodejsTest
+
+
---
+++ test.js
@@ -0,0 +1,5 @@
+﻿exports.testSomething = function(test){
+    test.expect(1);
+    test.ok(false, "this assertion should pass");
+    test.done();
+};
